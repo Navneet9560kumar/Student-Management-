1a3b9302c0efd0f6f3ad018a207cef711979caed
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2026-07-05 22:22:56 +0530

    fix: LF line endings in entrypoint.sh

--- student-backend/entrypoint.sh
+++ student-backend/entrypoint.sh
@@ -1,21 +1,4 @@
-# #!/bin/bash
 
-# echo "Waiting for PostgreSQL..."
-
-# # 'bd' ko change karke 'db' kar diya hai
-# while ! nc -z db 5432; do
-#   sleep 1
-# done
-
-# echo "PostgreSQL is up!"
-
-# echo "Running Alembic migrations..."
-
-# # Yahan se autogenerate hata diya hai, sirf upgrade command chalegi
-# alembic upgrade head
-
-# echo "Starting FastAPI server..."
-# uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 
 
 #!/bin/bash
