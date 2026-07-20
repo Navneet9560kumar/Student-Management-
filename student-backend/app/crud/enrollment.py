6ab1923df76ed3d3085e5066ffa111de6a63b147
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2026-07-20 12:21:29 +0530

    enrollment done

--- student-backend/app/crud/enrollment.py
+++ student-backend/app/crud/enrollment.py
@@ -2,36 +2,25 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.models.enrollment import Enrollment
 from app.schemas.enrollment import EnrollmentCreate
-from sqlalchemy.orm import joinedload
 
-async def create_enrollment(db: AsyncSession, data: EnrollmentCreate)-> Enrollment:
-      enrollment = Enrollment(
-            student_id = data.student_id,
-            course_id = data.course_id
-      )
-      db.add(enrollment)
-      await db.commit()
-      await db.refresh(enrollment)
-      return enrollment
+
+async def create_enrollment(db: AsyncSession, data: EnrollmentCreate) -> Enrollment:
+    enrollment = Enrollment(
+        student_id=data.student_id,
+        course_id=data.course_id
+    )
+    db.add(enrollment)
+    await db.commit()
+    await db.refresh(enrollment)
+    return enrollment
 
 async def get_all_enrollments(db: AsyncSession) -> list[Enrollment]:
-    result = await db.execute(
-         select(Enrollment)
-         .options(
-              joinedload(Enrollment.student), # Student ki details leyega
-              joinedload(Enrollment.course) #courses ki details layega
-           )
-         )
-    return result.unique().scalars().all()
-
-
+    
+    result = await db.execute(select(Enrollment))
+    return result.scalars().all()
 
 async def get_enrollments_by_student(db: AsyncSession, student_id: int) -> list[Enrollment]:
     result = await db.execute(
-        select(Enrollment)
-        .where(Enrollment.student_id == student_id)
-        .options(
-             joinedload(Enrollment.course)
-             )
+        select(Enrollment).where(Enrollment.student_id == student_id)
     )
-    return result.unique().scalars().all()
+    return result.scalars().all()
--- student-backend/app/schemas/enrollment.py
+++ student-backend/app/schemas/enrollment.py
@@ -5,12 +5,11 @@ from typing import Optional
 class EnrollmentCreate(BaseModel):
       student_id:int
       course_id: int
-
 class EnrollmentResponse(BaseModel):
     id: int
     student_id: int
     course_id: int
-    status: str
-    enrolled_at: datetime
+    status: Optional[str] = None          
+    enrolled_at: Optional[datetime] = None  
 
-    model_config = {"from_attributes": True}  # ← module_config nahi, model_config!
+    model_config = {"from_attributes": True}
